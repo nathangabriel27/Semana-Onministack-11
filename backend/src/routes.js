574a33f6c11c789f992f52616bc5ea14eb07bb35
Nathan Gabriel <nathangabriel27@gmail.com>
2020-03-27 23:38:10 -0300

    criando rota de listar apenas uma ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const OngsController = require('./controllers/OngsController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 routes.get('/ongs', OngsController.index)
 routes.post('/ongs', OngsController.create)
@@ -10,6 +11,8 @@ routes.post('/ongs', OngsController.create)
 routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
 routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.index)
 
 
 module.exports = routes
